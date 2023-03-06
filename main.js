978123ecabc6904a8d6cc303fc82de0a2f2f42e7
Stephen Wong <wongstephenky@gmail.com>
2023-03-06 12:26:46 -0800

    final commit

--- main.js
+++ main.js
@@ -1,33 +1,26 @@
-// target splash img and set breakpoint at 600px
 const hamburgerIcon = document.querySelector(".menu-ico");
 const closeIcon = document.querySelector(".close-ico");
 const grayNav = document.querySelector(".gray-nav");
 const nav = document.querySelector("nav");
 const navItem = document.querySelectorAll(".nav-item");
 
-function toggleNav() {
-  grayNav.classList.toggle("hidden");
-  nav.classList.toggle("hidden");
-}
-
-if (window.innerWidth > 600) {
-  navItem.removeEventListener;
-} else {
-  hamburgerIcon.addEventListener("click", () => {
-    toggleNav();
+const addListener = (el) => {
+  el.addEventListener("click", () => {
+    grayNav.classList.toggle("hidden");
+    nav.classList.toggle("hidden");
   });
+};
 
-  closeIcon.addEventListener("click", () => {
-    toggleNav();
-  });
+addListener(hamburgerIcon);
+addListener(closeIcon);
+addListener(grayNav);
+navItem.forEach((item) => {
+  addListener(item);
+});
 
-  navItem.forEach((item) => {
-    return item.addEventListener("click", () => {
-      toggleNav();
-    });
-  });
-
-  grayNav.addEventListener("click", () => {
-    toggleNav();
-  });
-}
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 600 && !grayNav.classList.contains("hidden")) {
+    grayNav.classList.toggle("hidden");
+    nav.classList.toggle("hidden");
+  }
+});
